8fafe7ba8837eafee6e6076e59b15f183134f7bc
Martins Olatunji <Martinsolatunji2@gmail.com>
2021-12-08 16:30:58 +0100

    Rewrote the script.js file and the index.html

---
+++ Quiz-App/js/quiz.js
@@ -0,0 +1,151 @@
+let quizData = [
+    {
+        question: "What does Bonasera want Don Corleone to do for him?",
+        a: "KILL THE BOYS WHO ROUGHED UP HIS DAUGHTER",
+        b: "LEND HIM MONEY",
+        c: "FIND HIS DAUGHTER A SUITABLE ITALIAN HUSBAND",
+        d: "FIND HIS DAUGHTER, WHO WAS MISSING",
+        correct: "a"
+    },
+
+    {
+        question: " Why does Don Corleone delay the family photograph at Connie's wedding?",
+        a: "SONNY WAS NOT THERE",
+        b: "THE PHOTO WOULD BE BACKLIT",
+        c: "THE DON HAD A MEETING TO GET TO",
+        d: "MICHAEL HAD NOT YET ARRIVED",
+        correct:"d",
+    },
+
+    
+    {
+        question: "Who are the men writing down license plate numbers outside Connie's wedding?",
+        a: "DON CORLEONE'S BODYGUARDS",
+        b: "NEIGHBORHOOD WATCH",
+        c: "FBI AGENTS",
+        d: "THE LOCAL POLICE",
+        correct:"c",
+    },
+    
+    {
+        question: "Who was Vincent Corleone?",
+        a: "The only member of the Corleone family that was not of their blood",
+        b: "Fredo's bastard",
+        c: "The Don",
+        d: "Connie's adopted child",
+        correct:"c",
+    },
+    
+    {
+        question: "Which of the following characters was NOT a guest at Connie and Carlo's wedding?",
+        a: "WOLTZ",
+        b: "FREDO",
+        c: "JOHNNY FONTANE",
+        d: "TOM HAGEN",
+        correct:"a",        
+    },
+    
+    {
+        question: "How much money did Woltz pay for Khartoum, his prized horse?",
+        a: "$6,000",
+        b: "$600,000",
+        c: "$6,000,000",
+        d: "$60,000",
+        correct:"b",
+    },
+    
+    {
+        question: "What does Woltz find in his bed the morning after his dinner with Tom Hagen?",
+        a: "THE SEVERED HOOFS OF HIS HORSE, KHARTOUM",
+        b: "A TICKING BOMB",
+        c: "HIS BIGGEST STAR'S EYEBALLS",
+        d: "THE SEVERED HEAD OF HIS HORSE, KHARTOUM",
+        correct:"d",
+    },
+    
+    {
+        question: " Who directed 'The Godfather' movies?",
+        a: "Francis Ford Coppola",
+        b: "Steven Spielberg",
+        c: " Mario Puzo",
+        d: "George Lucas",
+        correct:"c",
+    },
+
+    {
+        question: "When Paulie sees Connie collecting gifts for the bridal purse, he estimates aloud how much money is in it. Which of the following amounts would fall into the range of his estimate?",
+        a: "$45,000",
+        b: "$25,000",
+        c: "$15,000",
+        d: "$35,000",
+        correct:"b",
+        
+    },
+
+    {
+        question: "What was the name of the baker that made Connie and Carlo's wedding cake?",
+        a: "Enzo",
+        b: "Nazorine",
+        c: "Bonasera",
+        d: "Johhny",
+        correct:"b",        
+    },
+
+    {
+        question: "How many times was Vito shot by the Turk's men?",
+        a: "2",
+        b: "4",
+        c: "5",
+        d: "3",
+        correct:"c",
+    },
+
+    {
+        question: " What is the name of the baker that comes to the hospital to see Don Corleone?",
+        a: "Enzo",
+        b: "Sollozzo",
+        c: "Vincent",
+        d: "Johhny",
+        correct:"a",        
+    },
+   
+    {
+        question: "Tom Hagen, Don Corleone's lawyer and adopted son, tells him that Luca Brasi wants to see him. The Don asks if this is necessary. What is Tom's explanation for Luca's request to see him?",
+        a: "To offer friendship to the Don ",
+        b: "He had important business with the Don",
+        c: "Luca did not expect to be invited, and is grateful.",
+        d: "He just needed to see the Don",
+        correct:"c",
+ 
+    },
+
+    {
+        question: "Who made the statement- 'And I hope that their first child be a masculine child.'",
+        a: "Enzo",
+        b: "Johnny Fontane",
+        c: "Sollazo",
+        d: "Luca Brasi",
+        correct:"d",
+        
+    },
+
+    {
+        question: "What does Mike drop whilst running to the phone box after his dad has been shot?",
+        a: "the flowers",
+        b: "kay's hands",
+        c: "his newspaper",
+        d: "his popcorn",
+        correct:"c",
+    },
+
+    {
+        question: "What room number was Vito Corleone in at the hospital?",
+        a: "4",
+        b: "2",
+        c: "5",
+        d: "7",
+        correct:"b", 
+    },
+
+
+]
--- Quiz-App/js/script.js
+++ Quiz-App/js/script.js
@@ -1,210 +1,66 @@
-const quizData = [
-    {
-        question: "What does Bonasera want Don Corleone to do for him?",
-        a: "KILL THE BOYS WHO ROUGHED UP HIS DAUGHTER",
-        b: "LEND HIM MONEY",
-        c: "FIND HIS DAUGHTER A SUITABLE ITALIAN HUSBAND",
-        d: "FIND HIS DAUGHTER, WHO WAS MISSING",
-        correct: "a"
-    },
 
-    {
-        question: " Why does Don Corleone delay the family photograph at Connie's wedding?",
-        a: "SONNY WAS NOT THERE",
-        b: "THE PHOTO WOULD BE BACKLIT",
-        c: "THE DON HAD A MEETING TO GET TO",
-        d: "MICHAEL HAD NOT YET ARRIVED",
-        correct:"d",
-    },
-
-    
-    {
-        question: "Who are the men writing down license plate numbers outside Connie's wedding?",
-        a: "DON CORLEONE'S BODYGUARDS",
-        b: "NEIGHBORHOOD WATCH",
-        c: "FBI AGENTS",
-        d: "THE LOCAL POLICE",
-        correct:"c",
-    },
-    
-    {
-        question: "Who was Vincent Corleone?",
-        a: "The only member of the Corleone family that was not of their blood",
-        b: "Fredo's bastard",
-        c: "The Don",
-        d: "Connie's adopted child",
-        correct:"c",
-    },
-    
-    {
-        question: "Which of the following characters was NOT a guest at Connie and Carlo's wedding?",
-        a: "WOLTZ",
-        b: "FREDO",
-        c: "JOHNNY FONTANE",
-        d: "TOM HAGEN",
-        correct:"a",        
-    },
-    
-    {
-        question: "How much money did Woltz pay for Khartoum, his prized horse?",
-        a: "$6,000",
-        b: "$600,000",
-        c: "$6,000,000",
-        d: "$60,000",
-        correct:"b",
-    },
-    
-    {
-        question: "What does Woltz find in his bed the morning after his dinner with Tom Hagen?",
-        a: "THE SEVERED HOOFS OF HIS HORSE, KHARTOUM",
-        b: "A TICKING BOMB",
-        c: "HIS BIGGEST STAR'S EYEBALLS",
-        d: "THE SEVERED HEAD OF HIS HORSE, KHARTOUM",
-        correct:"d",
-    },
-    
-    {
-        question: " Who directed 'The Godfather' movies?",
-        a: "Francis Ford Coppola",
-        b: "Steven Spielberg",
-        c: " Mario Puzo",
-        d: "George Lucas",
-        correct:"c",
-    },
-
-    {
-        question: "When Paulie sees Connie collecting gifts for the bridal purse, he estimates aloud how much money is in it. Which of the following amounts would fall into the range of his estimate?",
-        a: "$45,000",
-        b: "$25,000",
-        c: "$15,000",
-        d: "$35,000",
-        correct:"b",
-        
-    },
-
-    {
-        question: "What was the name of the baker that made Connie and Carlo's wedding cake?",
-        a: "Enzo",
-        b: "Nazorine",
-        c: "Bonasera",
-        d: "Johhny",
-        correct:"b",        
-    },
-
-    {
-        question: "How many times was Vito shot by the Turk's men?",
-        a: "2",
-        b: "4",
-        c: "5",
-        d: "3",
-        correct:"c",
-    },
-
-    {
-        question: " What is the name of the baker that comes to the hospital to see Don Corleone?",
-        a: "Enzo",
-        b: "Sollozzo",
-        c: "Vincent",
-        d: "Johhny",
-        correct:"a",        
-    },
-   
-    {
-        question: "Tom Hagen, Don Corleone's lawyer and adopted son, tells him that Luca Brasi wants to see him. The Don asks if this is necessary. What is Tom's explanation for Luca's request to see him?",
-        a: "To offer friendship to the Don ",
-        b: "He had important business with the Don",
-        c: "Luca did not expect to be invited, and is grateful.",
-        d: "He just needed to see the Don",
-        correct:"c",
- 
-    },
-
-    {
-        question: "Who made the statement- 'And I hope that their first child be a masculine child.'",
-        a: "Enzo",
-        b: "Johnny Fontane",
-        c: "Sollazo",
-        d: "Luca Brasi",
-        correct:"d",
-        
-    },
-
-    {
-        question: "What does Mike drop whilst running to the phone box after his dad has been shot?",
-        a: "the flowers",
-        b: "kay's hands",
-        c: "his newspaper",
-        d: "his popcorn",
-        correct:"c",
-    },
-
-    {
-        question: "What room number was Vito Corleone in at the hospital?",
-        a: "4",
-        b: "2",
-        c: "5",
-        d: "7",
-        correct:"b", 
-    },
+let totalScore = 0
+let currentChosenOption = ''
+let currentQuestionNum = 1
+let restartQuiz = false
 
 
-]
-
-const quiz = document.getElementById("quiz")
-const answerE1s = document.querySelectorAll("answer")
-const question = document.getElementById("question")
-const a = document.getElementById("a")
-const b = document.getElementById("b")
-const c = document.getElementById("c")
-const d = document.getElementById("d")
-const submit = document.getElementById("submit")
-
-let currentQuiz = 0
-let score = 0
-
-loadQuiz()
-
-function loadQuiz() {
-    deselectAnswers()
-
-    const currentQuizData = quizData[currentQuiz]
-
-    question.innerText = currentQuizData.question
-    a_answer.innerText = currentQuizData.a
-    b_answer.innerText = currentQuizData.b
-    c_answer.innerText = currentQuizData.c
-    d_answer.innerText = currentQuizData.d
-}
-
-function deselectAnswers () {
-    answerE1s.forEach(answerE1 => answerE1.checked = false)
-}
-
-function getSelected() {
-    let answerE1
-    answerE1s.forEach(answerE1 => {
-        if (answerE1.checked) {
-            answer = answerE1.id
-        }
+const optionsTemplate = (questionBank,questionNum,option) =>{
+    let li = document.createElement("li");
+    li.innerHTML =  `
+            <input type='radio' name="options_${questionNum}" id="${option}"/>
+            <label for = "${option}">
+                ${questionBank[questionNum-1][option]}
+            </label>
+        `
+    li.addEventListener('click',function(){
+    currentChosenOption = option
     })
-    return answer
+    return li
 }
 
-submit.addEventListener("click", () => {
-    const answer= getSelected()
-    if(answer) {
-        if(answer === quizData[currentQuiz].correct) {
-            score++
-        }
 
-        currentQuiz++
+function quesitionTemplate(questionBank,questionNum){
+    return`
+       <div>
+           <h2>${questionBank[questionNum-1].question}</h2>
+           <ul id='questionContainer'>
+           </ul>
+       </div>
+    `
+}
 
-        if(currentQuiz < quizData.length) {
-            loadQuiz()
-        } else {
-            quiz.innerHTML = `<h2>You answered ${score}/${quizData.length} questions correctly</h2> <button onclick>"location.reload()"Reload</button>`
-            
-            
-        }
+function loadQuizToPage(questionBank,pageTag,questionNum,options=['a','b','c','d']){
+     pageTag.innerHTML = quesitionTemplate(questionBank,questionNum)
+     const questionContainer = document.getElementById('questionContainer')
+     options.forEach((val)=>{
+         questionContainer.appendChild(optionsTemplate(questionBank,questionNum,val))
+     })
+}
+
+
+function submitResult(){
+   
+    try{
+        currentChosenOption===quizData[currentQuestionNum-1].correct ? totalScore += 1: totalScore += 0
+    }catch(e){
+        submit.innerHTML = 'Submit'
+        totalScore = 0
     }
-})
+    currentChosenOption = ''
+    currentQuestionNum += 1
+    if (currentQuestionNum === quizData.length+1){
+        currentQuestionNum = 0
+        questions.innerHTML = `<h2>You answered ${totalScore}/${quizData.length} questions correctly</h2>`
+        submit.innerHTML = 'Restart Quiz' 
+        restartQuiz = true
+        console.log(restartQuiz)
+        return
+    }
+    restartQuiz ? loadQuizToPage(quizData,questions,1):loadQuizToPage(quizData,questions,currentQuestionNum)
+    restartQuiz = false
+}
+
+let submit = document.getElementById('submit')
+let questions = document.getElementById('questions')
+loadQuizToPage(quizData,questions,1)
